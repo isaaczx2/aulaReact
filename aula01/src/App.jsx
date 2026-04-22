e6a1c6809ccb219d0b35a1e0fd5cf4ca64d7aa67
isaaczx2 <isaac14230709@gmail.com>
2026-04-22 14:58:36 -0300

    22/4

---
+++ aula01/src/App.jsx
@@ -0,0 +1,29 @@
+import"./App.css";
+
+export default function App()
+{
+let nome,idade,altura
+
+nome="isaac";
+idade=15;
+altura=1.88;
+dias=idade*365
+
+
+
+  return (
+<div>
+  <h1> ola mundo!!!</h1>
+
+
+<div className="conteudo">
+<p> olá seja bem vindo ao react com js.</p>
+</div>
+
+</div>
+
+)
+}
+
+
+
